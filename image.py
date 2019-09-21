bfc9e2e6d4da89bf24c6041bd1659a2085bb075f
Caleb Thorsteinson <caleb@thorsteinson.io>
2019-09-21 00:10:32 -0700

    Moves image manipulation into a dedicated class
    Creates an image class that encapsulates image manipulations and
    dealing with opencv directly. Now we can just make an image and apply
    some transformations through a much, much better interface, with less
    parameters and probably much more safetey as well. This implements
    most of the functionality that was in our test file, but there's still
    more to be done, and of course tested.

---
+++ image.py
@@ -0,0 +1,162 @@
+import cv2 as cv
+import numpy as np
+from pathlib import Path
+
+
+class Image:
+    # Allow an image to be constructed from either a path that
+    # references an image or an array literal
+    def __init__(self, image):
+        if isinstance(image, Path):
+            self.img = cv.imread(str(path.resolve()))
+        elif isinstance(ndarray, image):
+            self.img = image
+        else:
+            raise TypeError
+
+    def __copy__(self):
+        return copy(self.img)
+
+    # Looks at number of dimensions to determine whether the image is
+    # color or grayscale in colorspace
+    def is_gray(self):
+        if self.img.shape[2] == 1:
+            return true
+        elif self.img.shape[2] == 3:
+            return false
+        raise ValueError
+
+    # Converts the image to grayscale
+    def grayscale(self):
+        self.img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # Converts image to BGR colorspace
+    def bgr_color(self):
+        self.img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+
+    # Takes a binary image, and applies a threshold on sections of pixels
+    # that don't meet a given area. This can effectively filter bits of
+    # noise from actual blocks of text we wish to capture on the board
+    def area_threshold(self, area):
+        # Keep track of each coordinate and the set that it belongs to
+        regionMap = {}
+
+        # Iterate through all coordinates of the image
+        for (x, y), n in np.ndenumerate(self.img):
+            # Only black pixels
+            if n == 0:
+                leftNeighbor = (x - 1, y)
+                upNeighbor = (x, y - 1)
+
+                region = set()
+
+                if leftNeighbor[0] >= 0 and leftNeighbor in regionMap:
+                    region = regionMap[leftNeighbor]
+
+                elif upNeighbor[1] >= 0 and upNeighbor in regionMap:
+                    region = regionMap[upNeighbor]
+
+                regionMap[(x, y)] = region
+                region.add((x, y))
+
+        # Create a new a array of the same shape
+        filtered = np.zeros_like(img)
+
+        WHITE = 255
+        for (x, y), n in np.ndenumerate(img):
+            if n == 0 and len(regionMap[(x, y)]) < area:
+                n = WHITE
+
+            filtered[x, y] = n
+
+        self.img = filtered
+
+    def crop_border(self, percent):
+        y = self.img.shape[0]
+        y_offset = round(y * percent)
+        y_size = y - y_offset * 2
+        x = self.img.shape[1]
+        x_offset = round(x * percent)
+        x_size = x - x_offset * 2
+
+        self.img = np.array(
+            self.img[y_offset : y_offset + y_size, x_offset : x_offset + x_size]
+        )
+
+    # Returns an image that's projected from the 4 given points
+    def perspective_transform(self, points):
+        top_left = points[0]
+        top_right = points[1]
+        bottom_right = points[2]
+        bottom_left = points[3]
+
+        source = np.array(
+            [top_left, top_right, bottom_right, bottom_left], dtype="float32"
+        )
+        maxW = max(top_right[0] - top_left[0], bottom_right[0] - bottom_left[0])
+        maxH = max(bottom_right[1] - top_right[1], bottom_left[1] - top_left[1])
+        # This determines the output size we want to map onto. It's not
+        # perfect, but it's good enough to get the general shape of what
+        # was captured
+        dest = np.array(
+            [(0, 0), (maxW - 1, 0), (maxW - 1, maxH - 1), (0, maxH - 1)],
+            dtype="float32",
+        )
+        transM = cv.getPerspectiveTransform(source, dest)
+
+    self.img = cv.warpPerspective(self.img, transM, (maxW, maxH))
+
+    def adaptive_threshold(self, block_size, c):
+        if block_size % 2 != 1 or block_size <= 1:
+            raise ValueError
+
+        self.img = cv.adaptiveThreshold(
+            self.img,
+            # Max value that will be output, keep this to make image
+            # purely black or white
+            255,
+            cv.ADAPTIVE_THRESH_GAUSSIAN_C,  # Use Guassion method
+            cv.THRESH_BINARY,  # Threshold Type
+            block_size,
+            c,
+        )  # Constant that is subtracted during thresholding
+
+    def blur(self, kernel_size):
+        self.img = cv.blur(self.img, (kernel_size, kernel_size))
+
+    def threshold(self, percent_black):
+        _, self.img = cv.threshold(
+            self.img, 255 - int(percent_black * 255), 255, cv.THRESH_BINARY
+        )
+
+    # It's nice to have these properties, because it can be easy to
+    # forget that y is the first value in the shape and x is the 2nd
+    @property
+    def x_res(self):
+        return self.img.shape[1]
+
+    @property
+    def y_res(self):
+        return self.img.shape[0]
+
+    # Writes a watermark to the bottom left corner of an image. The
+    # water mark consists of the keys and values in the dictionary passed
+    def watermark(self, dictionary):
+        if self.is_gray():
+            self.bgr_color()
+
+        (_, line_height), _ = cv.getTextSize("TEXT", cv.FONT_HERSHEY_DUPLEX, 1.0, 0)
+
+        x = round(self.x_res * 0.05)
+        y = round(self.yres - self.y_res * 0.05)
+
+        for k, v in dictionary.items():
+            self.img = cv.putText(
+                self.img,
+                "{}: {}".format(k, v),
+                (xP, yP),
+                cv.FONT_HERSHEY_DUPLEX,
+                1,
+                (192, 36, 27),
+            )
+            y -= round(line_height * 1.1)
