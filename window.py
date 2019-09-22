f086cb63d1cbc76892092350b6cd91c64c38b26d
Caleb Thorsteinson <caleb@thorsteinson.io>
2019-09-21 18:41:56 -0700

    Modifies callbacks so data is optional
    If data isn't provided, the callbacks will still work. This is useful
    if the callback can be as simple as a labmda. Otherwise the data will
    be passed in.

--- window.py
+++ window.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import sys
 from image import Image
 
 
@@ -18,10 +19,14 @@ SPECIAL_KEYS = {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_SPACE, KEY_ENTER, KEY
 
 # Modifies a click handler that the handler will work with OpenCV
 # style callbacks.
-def cv_click_callback(event, x, y, flags, handler):
+def cv_click_callback(event, x, y, flags, params):
     # Fire only when the button is lifted up
     if event == cv.EVENT_LBUTTONUP:
-        handler(x, y)
+        handler, data = params
+        if data:
+            handler(x, y, data)
+        else:
+            handler(x, y)
 
 
 # Adds a control layer for the OpenCV window and event
@@ -31,25 +36,35 @@ class Window:
         cv.namedWindow(WINDOW_NAME)
         self.click_handler = None
         self.exit = False
+        # By default, our quit handler just spits out false, no matter what
+        self.quit_handler = lambda _: False
+        self.quit_data = None
         # A dictionary that maps keycodes to handles
         # By default, map q and ESC to quit the program
-        self.key_handlers = {ord("q"): self.close, KEY_ESC: self.close}
+        self.key_handlers = {ord("q"): (sys.exit, None), KEY_ESC: (sys.exit, None)}
 
+    # Run will basically stay in this loop until some exit condition
+    # has been reached through the quit_on callback, or through close
+    # being called on the window.
     def run(self):
-        while not self.exit:
+        while not (self.exit or self.quit_handler(self.quit_data)):
             k = cv.waitKey(WAIT_PERIOD)
-            handler = self.key_handlers.get(k)
-            if handler:
-                handler()
+            pair = self.key_handlers.get(k)
+            if pair:
+                handler, data = pair
+                if data:
+                    handler(data)
+                else:
+                    handler()
 
-    def handle_click(self, handler):
+    def handle_click(self, handler, data=None):
         if not callable(handler):
             raise ValueError
 
         self.click_handler = handler
-        cv.setMouseCallback(WINDOW_NAME, cv_click_callback, handler)
+        cv.setMouseCallback(WINDOW_NAME, cv_click_callback, (handler, data))
 
-    def handle_key(self, k, handler):
+    def handle_key(self, k, handler, data=None):
         if type(k) == int and k not in SPECIAL_KEYS:
             # Not one of the special keys we have codes for
             raise ValueError
@@ -61,7 +76,11 @@ class Window:
         if not callable(handler):
             raise ValueError
 
-        self.key_handlers[k] = handler
+        self.key_handlers[k] = (handler, data)
+
+    def quit_on(self, quit_handler, data):
+        self.quit_handler = quit_handler
+        self.quit_data = data
 
     def close(self):
         self.exit = True
