eb329f4310d7ab1fb085d9f129a96f967904e512
Caleb Thorsteinson <caleb@thorsteinson.io>
2019-09-29 17:57:35 -0700

    Modifies window class to use asyncio
    Almost entirely overhauls the class so that it's coroutine based.
    
    The approach here is to avoid dealing with callbacks entirely, and
    instead wait on clicks and key presses.  This will hopefully lend some
    readability to any code that manages the window, since it will appear
    to look more synchronous.

--- window.py
+++ window.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import sys
+import asyncio
 from image import Image
 
 
@@ -17,16 +18,7 @@ KEY_ESC = 27
 
 SPECIAL_KEYS = {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_SPACE, KEY_ENTER, KEY_ESC}
 
-# Modifies a click handler that the handler will work with OpenCV
-# style callbacks.
-def cv_click_callback(event, x, y, flags, params):
-    # Fire only when the button is lifted up
-    if event == cv.EVENT_LBUTTONUP:
-        handler, data = params
-        if data:
-            handler(x, y, data)
-        else:
-            handler(x, y)
+NULL_CODE = 255
 
 
 # Adds a control layer for the OpenCV window and event
@@ -34,67 +26,57 @@ def cv_click_callback(event, x, y, flags, params):
 class Window:
     def __init__(self):
         cv.namedWindow(WINDOW_NAME)
-        self.click_handler = None
-        self.exit = False
-        # By default, our quit handler just spits out false, no matter what
-        self.quit_handler = lambda: False
-        self.quit_data = None
-        # A dictionary that maps keycodes to handles
-        # By default, map q and ESC to quit the program
-        self.key_handlers = {ord("q"): (sys.exit, None), KEY_ESC: (sys.exit, None)}
 
-    # Run will basically stay in this loop until some exit condition
-    # has been reached through the quit_on callback, or through close
-    # being called on the window.
-    def run(self):
-        while not self.exit:
-            if self.quit_data:
-                quit = self.quit_handler(self.quit_data)
-            else:
-                quit = self.quit_handler()
-            if quit:
-                break
+        self.loop = asyncio.get_event_loop()
 
-            k = cv.waitKey(WAIT_PERIOD)
-            pair = self.key_handlers.get(k)
-            if pair:
-                handler, data = pair
-                if data:
-                    handler(data)
-                else:
-                    handler()
+        self.pos_fut = self.loop.create_future()
 
-    def handle_click(self, handler, data=None):
-        if not callable(handler):
-            raise ValueError
+        def cv_click_callback(event, x, y, flags, params):
+            # Fire only when the button is lifted up
+            if event == cv.EVENT_LBUTTONUP:
+                if self.pos_fut.done():
+                    # Reset future if it was already done. Don't
+                    # return old clicks
+                    self.pos_fut = self.loop.create_future()
+                self.pos_fut.set_result((x, y))
 
-        self.click_handler = handler
-        cv.setMouseCallback(WINDOW_NAME, cv_click_callback, (handler, data))
+        cv.setMouseCallback(WINDOW_NAME, cv_click_callback, None)
 
-    def handle_key(self, k, handler, data=None):
-        if type(k) == int and k not in SPECIAL_KEYS:
-            # Not one of the special keys we have codes for
-            raise ValueError
-        # Need to organize and fix this disaster
-        elif k in SPECIAL_KEYS:
-            k = k
-        elif type(k) == str:
-            k = ord(k)
-        else:
-            raise TypeError
+        self.key_fut = self.loop.create_future()
 
-        if not callable(handler):
-            raise ValueError
+    def run(self, coro):
+        # Drives the events through the usage of waitkey. Without it,
+        # no events will ever be registered
+        def driver():
+            key_code = cv.waitKey(WAIT_PERIOD)
+            if key_code != NULL_CODE:
+                if self.key_fut.done():
+                    # Reset our future, so old keys aren't sent
+                    self.key_fut = self.loop.create_future()
+                self.key_fut.set_result(key_code)
 
-        self.key_handlers[k] = (handler, data)
+            # Keep calling this, driving the main event loop
+            self.loop.call_soon(driver)
 
-    def quit_on(self, quit_handler, data=None):
-        self.quit_handler = quit_handler
-        self.quit_data = data
-
-    def close(self):
-        self.exit = True
+        self.loop.call_soon(driver)
+        self.loop.run_until_complete(coro)
         cv.destroyAllWindows()
+
+    # Returns the X, Y coordinates of the position clicked
+    async def click(self):
+        p = await self.pos_fut
+        # Reset the future for the next click
+        self.pos_fut = self.loop.create_future()
+        return p
+
+    # Returns the keycode, or the character pressed if possible
+    async def keypress(self):
+        key_code = await self.key_fut
+        self.key_fut = self.loop.create_future()
+        if key_code in SPECIAL_KEYS:
+            return key_code
+        else:
+            return chr(key_code)
 
     def show(self, img):
         if isinstance(img, Image):
