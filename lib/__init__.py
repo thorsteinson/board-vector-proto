636f2a78abbb14ed88378393b9c48e579247b3ad
Caleb Thorsteinson <caleb@thorsteinson.io>
2019-09-30 11:34:54 -0700

    Adds types to __init__

--- lib/__init__.py
+++ lib/__init__.py
@@ -5,14 +5,14 @@ _mgr = None
 _win = None
 
 
-def get_asset_mgr():
+def get_asset_mgr() -> asset_manager.AssetManager:
     global _mgr
     if not _mgr:
         _mgr = asset_manager.AssetManager()
     return _mgr
 
 
-def get_window():
+def get_window() -> window.Window:
     global _win
     if not _win:
         _win = window.Window()
